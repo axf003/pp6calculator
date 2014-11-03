85b53121411da8e3812aea566c743737ab3d0903
Andrew Foster <a.foster@cern.ch>
2014-11-03 14:49:35 +0000

    Adding ex1&2 cpp tasks

---
+++ Cpp_exercises/ex2/ex2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+
+  double a,b,c;
+  std::cout << "Input a number: " << std::endl;
+  std::cin >> a;
+  std::cout << "Input another number: " << std::endl;
+  std::cin >> b;
+  c=a*b;
+  std::cout << "Their product = " << c << std::endl;
+  
+
+return 0;
+}
